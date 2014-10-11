dc730c1ada27461e6837e60c4bad83afcbfe7afc
uyoung <young7007@gmail.com>
2014-10-11 23:34:01 +0800

    init

---
+++ conf/db.php
@@ -0,0 +1,11 @@
+<?php
+return array (
+  'DB_TYPE' => 'mysql',
+  'DB_HOST' => 'localhost',
+  'DB_PORT' => '3306',
+  'DB_NAME' => 'vmkt',
+  'DB_USER' => 'vmkt',
+  'DB_PWD' => 'marketing',
+  'DB_PREFIX' => 'tp_',
+);
+?>
